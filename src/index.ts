060ceb612203f93bb30f21e420cbd5db775559ef
orothy579 <ftsky@naver.com>
2025-02-18 08:19:33 +0000

    prettier

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
-import { SerialPort, ReadlineParser } from "serialport";
+import { SerialPort, ReadlineParser } from 'serialport'
 import mqtt from 'mqtt'
-
 
 // Create a port
 const port = new SerialPort({
-  path: "/dev/ttyUSB0",
+  path: '/dev/ttyUSB0',
   baudRate: 9600,
-});
+})
 
-port.write("hello");
+port.write('hello')
 
 port.open(() => {
-  console.log("Port opened");
-});
+  console.log('Port opened')
+})
 
-port.on("data", function (data: Buffer) {
-  console.log("Data:", data.toString());
-});
+port.on('data', function (data: Buffer) {
+  console.log('Data:', data.toString())
+})
--- src/modbus.ts
+++ src/modbus.ts
@@ -1,21 +1,21 @@
-import ModbusRTU from 'modbus-serial';
+import ModbusRTU from 'modbus-serial'
 
 async function connectModbus() {
-  const client = new ModbusRTU();
+  const client = new ModbusRTU()
 
   try {
-    await client.connectRTUBuffered('/dev/ttyUSB0', { baudRate: 9600 }); // Connected to serial port
-    console.log('Modbus connection success');
+    await client.connectRTUBuffered('/dev/ttyUSB0', { baudRate: 9600 }) // Connected to serial port
+    console.log('Modbus connection success')
 
-    client.setID(1); // set slave ID
+    client.setID(1) // set slave ID
 
     // reading Data : holding register에서 0번부터 125(최대)개의 값 읽기
-    const data = await client.readHoldingRegisters(0, 125);
-    console.log('Data:', data.data);
+    const data = await client.readHoldingRegisters(0, 125)
+    console.log('Data:', data.data)
   } catch (error) {
-    console.error('Error:', error);
+    console.error('Error:', error)
   } finally {
-    client.close();
+    client.close()
   }
 }
 
