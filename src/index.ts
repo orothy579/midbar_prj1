6459f52aa397a6739210b09e64d2aa32713b3a7f
orothy579 <ftsky2050@naver.com>
2025-02-19 00:50:45 +0000

    modbus and mqtt

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,16 @@ import mqtt from 'mqtt'
 
 // Create a port
 const port = new SerialPort({
-  path: '/dev/ttyUSB0',
-  baudRate: 9600,
+    path: '/dev/ttyUSB0',
+    baudRate: 9600,
 })
 
 port.write('hello')
 
 port.open(() => {
-  console.log('Port opened')
+    console.log('Port opened')
 })
 
 port.on('data', function (data: Buffer) {
-  console.log('Data:', data.toString())
+    console.log('Data:', data.toString())
 })
--- src/modbus.ts
+++ src/modbus.ts
@@ -1,11 +1,11 @@
 import dotenv from 'dotenv'
-require('dotenv').config()
+dotenv.config()
 import ModbusRTU from 'modbus-serial'
 import mqtt from 'mqtt'
 
 const MQTT_BROKER_IP = process.env.MQTT_BROKER_IP || 'localhost'
-const MQTT_URL = `mqtt://{MQTT_BROKER_IP}:1883`
-const ACCESS_TOKEN = process.env.ACCESS_TOKEN // device access token
+const MQTT_URL = `mqtt://${MQTT_BROKER_IP}:1883`
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN
 
 const SERIAL_PORT = '/dev/ttyUSB0'
 const BAUD_RATE = 9600
@@ -13,43 +13,60 @@ const SLAVE_ID = 1
 const REGISTER_START = 0
 const REGISTER_COUNT = 125
 
+// mqttClient 생성
 const mqttClient = mqtt.connect(MQTT_URL, {
     username: ACCESS_TOKEN,
 })
 
-async function connectModbus() {
-    const modbusClient = new ModbusRTU()
+// Modbus 클라이언트 생성
+const modbusClient = new ModbusRTU()
 
+async function initModbus() {
     try {
-        await modbusClient.connectRTUBuffered(SERIAL_PORT, { baudRate: BAUD_RATE }) // Connected to serial port
-        console.log('Modbus connection success')
+        if (!modbusClient.isOpen) {
+            await modbusClient.connectRTUBuffered(SERIAL_PORT, { baudRate: BAUD_RATE })
+            console.log('Modbus connection successed')
+            modbusClient.setID(SLAVE_ID)
+        }
+    } catch (error) {
+        console.error('Modbus connection error:', error)
+    }
+}
 
-        modbusClient.setID(SLAVE_ID) // set slave ID
-
-        // reading Data : holding register에서 0번부터 125(최대)개의 값 읽기
+async function readModbusData() {
+    try {
+        if (!modbusClient.isOpen) {
+            console.log('Modbus connection lost. Reconnecting...')
+            await initModbus()
+        }
+        // Reading holding register
         const data = await modbusClient.readHoldingRegisters(REGISTER_START, REGISTER_COUNT)
-        console.log("Data:", data.data)
+        console.log('Data:', data.data)
 
         const payload = JSON.stringify({
             data: data.data,
         })
 
         mqttClient.publish('v1/devices/me/telemetry', payload, () => {
-            console.log(`Published:${payload}`)
+            console.log(`Published: ${payload}`)
         })
     } catch (error) {
-        console.error('Error:', error)
-    } finally {
-        modbusClient.close()
+        console.error('Error reading modbus data:', error)
     }
 }
 
-setInterval(connectModbus, 1000)
+async function startPolling() {
+    await initModbus()
+    setInterval(readModbusData, 1000)
+}
+
+// master로서 slave 의 data polling 시도
+startPolling()
 
 mqttClient.on('connect', () => {
-    console.log('connected to thingsboard mqtt')
+    console.log('Connected to ThingsBoard MQTT')
 })
 
 mqttClient.on('error', (err) => {
-    console.error('Mqtt error:', err)
+    console.error('MQTT error:', err)
 })
