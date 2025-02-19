59f97f7fa9c9a0371ebba0de31abcd3383f59d84
orothy579 <ftsky@naver.com>
2025-02-19 00:24:46 +0000

    aa

--- src/modbus.ts
+++ src/modbus.ts
@@ -14,44 +14,42 @@ const REGISTER_START = 0
 const REGISTER_COUNT = 125
 
 const mqttClient = mqtt.connect(MQTT_URL, {
-  username: ACCESS_TOKEN,
+    username: ACCESS_TOKEN,
 })
 
 async function connectModbus() {
-  const modbusClient = new ModbusRTU()
+    const modbusClient = new ModbusRTU()
 
-  try {
-    await modbusClient.connectRTUBuffered(SERIAL_PORT, { baudRate: BAUD_RATE }) // Connected to serial port
-    console.log('Modbus connection success')
+    try {
+        await modbusClient.connectRTUBuffered(SERIAL_PORT, { baudRate: BAUD_RATE }) // Connected to serial port
+        console.log('Modbus connection success')
 
-    modbusClient.setID(SLAVE_ID) // set slave ID
+        modbusClient.setID(SLAVE_ID) // set slave ID
 
-    // reading Data : holding register에서 0번부터 125(최대)개의 값 읽기
-    const data = await modbusClient.readHoldingRegisters(REGISTER_START, REGISTER_COUNT)
-    console.log('Data:', data.data)
+        // reading Data : holding register에서 0번부터 125(최대)개의 값 읽기
+        const data = await modbusClient.readHoldingRegisters(REGISTER_START, REGISTER_COUNT)
+        console.log('Data:', data.data)
 
-    const payload = JSON.stringify({
-      data: data.data
-    })
+        const payload = JSON.stringify({
+            data: data.data,
+        })
 
-    mqttClient.publish('v1/devices/me/telemetry', payload, () => {
-      console.log(`Published:${payload}`)
-    })
-
-  } catch (error) {
-    console.error('Error:', error)
-  } finally {
-    modbusClient.close()
-  }
+        mqttClient.publish('v1/devices/me/telemetry', payload, () => {
+            console.log(`Published:${payload}`)
+        })
+    } catch (error) {
+        console.error('Error:', error)
+    } finally {
+        modbusClient.close()
+    }
 }
 
 setInterval(connectModbus, 1000)
 
-
 mqttClient.on('connect', () => {
-  console.log('connected to thingsboard mqtt')
+    console.log('connected to thingsboard mqtt')
 })
 
 mqttClient.on('error', (err) => {
-  console.error('Mqtt error:', err)
+    console.error('Mqtt error:', err)
 })
